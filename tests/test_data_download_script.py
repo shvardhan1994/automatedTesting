511b5119ba767b13984abee4faeab43b97f2e8d2
shvardhan1994 <shvardhan1994@gmail.com>
2022-12-09 15:17:44 +0100

    modified test scripts

--- tests/test_data_download_script.py
+++ tests/test_data_download_script.py
@@ -3,13 +3,12 @@ import os
 import shutil
 
 def test_script():
-
+    shutil.rmtree("data")
     script_path = os.path.abspath("scripts/download_data.py")    
     runpy.run_path(script_path)
 
     out_file = os.path.abspath("data/raw/titanic.csv")
     assert os.path.exists(out_file), "File does not exist!"
 
-    shutil.rmtree("data")
 
     
